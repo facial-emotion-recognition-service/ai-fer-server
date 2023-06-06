827d803844b9de13a71e90cfabb9bcf14d86815d
Or Bartal <orbartal@gmail.com>
2023-06-05 20:27:18 -0400

    Append packages to path in server main

--- human_emotion/server/main_server.py
+++ human_emotion/server/main_server.py
@@ -1,6 +1,7 @@
 import os, sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append('/emosense/server')
+sys.path.append('/emosense/core')
 
 from app_config_provider import AppConfigProvider
 from model_config_provider import ModelConfigProvider
